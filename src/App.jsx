7d13afbc9c11cb553ff9f0e8d50143f1800bb6e7
Tonio <tonio.suessdorf@gmail.com>
2023-12-12 09:56:25 +0100

    Add TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import React from 'react'
-import './App.css'
-
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -27,20 +27,46 @@ function App() {
       <div className="flex" onClick={handleClick}>
         <div className="flex-initial w-32 font-bold underline decoration-sky-500 text-black dark:text-yellow-300">
           <label className="cursor-pointer grid place-items-center">
-            <input type="checkbox" value="synthwave" className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2" />
-            <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
-            <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+            <input
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+            />
+            <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <circle cx="12" cy="12" r="5" />
+              <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+            </svg>
+            <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+            </svg>
           </label>
         </div>
-      </div >
-      <div>
-        <ol className='justify-center flex-col items-center m-4'>
-          <li className='p-2 dark:bg-gray-700 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 '>Test</li>
-          <li className='p-2 dark:bg-gray-700 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 '>Test</li>
-        </ol>
+        <TodoList />
       </div>
     </>
   );
 }
 
-export default App
+export default App;
