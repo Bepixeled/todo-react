955c34a3272ec3a598afdbf6925ab8173f60420e
Tonio <tonio.suessdorf@gmail.com>
2023-12-12 14:12:59 +0100

    Refactor TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { useState } from "react";
 
 const TodoList = () => {
-  const [todoItems, setTodoItems] = useState([]);
+  const [todoItems, setTodoItems] = useState([
+    {
+      id: 0,
+      text: "Enter text...",
+      done: false,
+    },
+  ]);
 
   return (
-    <div className="shadow-md rounded max-w-xl mx-auto mt-8 pt-6 pb-8 mb-4">
+    <div className="rounded overflow-hidden max-w-xl mx-auto mt-8 pt-6 pb-8 mb-4">
       <ol className="justify-center flex-col items-center m-4">
-        <li className="p-2 dark:bg-dark-secondary-700 bg-light-primary-100 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 ">
-          Enter text...
-        </li>
         {todoItems.map((item, index) => {
           <li key={index}>
             <p>
