50f4fb58be38d281963a6c85182e1d95af902c7a
Tonio <tonio.suessdorf@gmail.com>
2023-12-12 11:51:08 +0100

    Integrate TodoList component into App component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 const TodoList = () => {
+  const [todoItems, setTodoItems] = useState([]);
+
   return (
-    <div className="max-w-lg mx-auto mt-8">
-      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="max-w-xl mx-auto mt-8">
+      <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h2 className="text-2xl font-bold mb-4">Todo List</h2>
 
         <ol className="justify-center flex-col items-center m-4">
-          <li className="p-2 dark:bg-gray-700 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 ">
+          <li className="p-2 dark:bg-dark-secondary-200 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 ">
             Test
           </li>
-          <li className="p-2 dark:bg-gray-700 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 ">
+          <li className="p-2 dark:bg-dark-secondary-700 border-b-2 border-solid dark:border-blue-200 bg-gray-200 border-gray-700 w-9/12 ">
             Test
           </li>
+          {todoItems.map((item, index) => {
+            <li key={index}>
+              <p>
+                {index}: {item}
+              </p>
+            </li>;
+          })}
         </ol>
       </div>
     </div>
