17f94e5940021c877113a55e6b80c39904d8e71d
workingj <workingj@pm.me>
2023-12-15 14:37:42 +0100

    add deletefunciton to delete Button

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Listitem from "./Listitem";
 
-import Y from "./Y";
-import X from "./x";
-
 const TodoList = ({
   todoItems,
   onChangeItem,
@@ -12,22 +9,20 @@ const TodoList = ({
   onDeleteTodoItem,
 }) => {
   return (
-    <div className="rounded overflow-hidden mx-auto mt-8 w-5/6 md:w-3/5 flex justify-center ">
-      <X/>
-          <ol className="flex justify-center flex-col items-center mb-10 w-full">
-            {todoItems.map((item) => {
-              return (
-                <Listitem
-                  key={item.id}
-                  item={item}
-                  onChangeItem={onChangeItem}
-                  onNewItem={onNewItem}
-                  onDeleteTodoItem={onDeleteTodoItem}
-                />
-              );
-            })}
-          </ol>
-          <Y/>
+    <div className="rounded overflow-hidden mx-auto mt-8 w-5/6 md:w-3/5 flex justify-center">
+      <ol className="flex justify-center flex-col items-center mb-10 w-full">
+        {todoItems.map((item) => {
+          return (
+            <Listitem
+              key={item.id}
+              item={item}
+              onChangeItem={onChangeItem}
+              onNewItem={onNewItem}
+              onDeleteTodoItem={onDeleteTodoItem}
+            />
+          );
+        })}
+      </ol>
     </div>
   );
 };
