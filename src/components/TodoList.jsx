e6f8de331ab1ddb4b9af7c08f07f489d0fd92d1c
Bepixeled <git@bepixeled.dev>
2023-12-15 15:17:38 +0100

    styling

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,8 +9,8 @@ const TodoList = ({
   onDeleteTodoItem,
 }) => {
   return (
-    <div className="rounded overflow-hidden mx-auto mt-8 w-5/6 md:w-3/5 flex justify-center">
-      <ol className="flex justify-center flex-col items-center mb-10 w-full">
+    <div className="rounded overflow-hidden mx-auto mt-8 w-96 md:w-3/5 flex flrx-col justify-center">
+      <ol className="flex justify-center flex-col items-center mb-10 w-96">
         {todoItems.map((item) => {
           return (
             <Listitem
